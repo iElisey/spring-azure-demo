9049821532db504987a05a3836f8c5800a4f9760
iElisey <yelisei.osadchyi@gmail.com>
2024-04-26 21:30:04 +0300

    second commit

--- src/main/java/org/elos/springazuredemo/SpringAzureDemoApplication.java
+++ src/main/java/org/elos/springazuredemo/SpringAzureDemoApplication.java
@@ -2,12 +2,19 @@ package org.elos.springazuredemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+
 public class SpringAzureDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringAzureDemoApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAzureDemoApplication.class, args);
+    }
+    @GetMapping("/message")
+    public String message(){
+        return "Congrats ! your application deployed successfully in Azure Platform. !";
+    }
 }
